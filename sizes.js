b786660bbe993d2a6805a9e49fe9db75f08ea3cd
emcall <esm328@nyu.edu>
2015-02-18 16:05:03 -0500

    Messing with the wings and label positioning
    Wound up mostly breaking things rather than fixing them; I may need to
    redo the way I handle the wings entirely.

--- sizes.js
+++ sizes.js
@@ -25,21 +25,16 @@ var newDragon = function(event){
 	//initial positioning
 	dragon.style.top = "50px";
 	dragon.style.left = "500px";
+	dragon.style.zIndex = 1;
 	
 	//add image
 	var dragonImage = document.createElement("img");
 	dragonImage.src = "images/" + d_breed + "_body.png";	
 	dragonImage.height = d_length; 
+	dragonImage.zIndex = 1;
 	dragon.appendChild(dragonImage);
 	dragonImage.addEventListener('mousedown', move);
 	
-	//add textxtxt
-	var label = document.createElement("p");
-	label.textContent = d_name;
-	label.className = "label";
-	label.style.top = d_length/6;
-	
-	dragon.appendChild(label);
 	
 	//wing div
 	var wings = document.createElement('div');
@@ -47,7 +42,7 @@ var newDragon = function(event){
 	//position the wing to center
 	
 	wings.style.left = -d_wingspan/2 + d_length/6;
-	wings.style.zIndex = "-1";
+	//wings.style.zIndex = "-1";
 	wings.style.top = 0;
 	
 	//this pushes the wings down the shoulders
@@ -57,7 +52,23 @@ var newDragon = function(event){
 	var wingImage = document.createElement("img");
 	wingImage.src = "images/" + d_breed + "_wings.png";	
 	wingImage.width = d_wingspan; 
+	wingImage.style.zIndex = "-1";
 	wings.appendChild(wingImage);	
+	dragon.appendChild(wings);
+	
+	
+	//add textxtxt
+	var label = document.createElement("p");
+	label.textContent = d_name;
+	label.className = "label";
+	label.style.top = d_length/6;
+	label.style.width = d_wingspan;
+	label.style.zIndex= 2;
+	
+	wings.appendChild(label);
+	
+	wingImage.addEventListener('mousedown', moveWing);
+	
 	dragon.appendChild(wings);
 	
 	
@@ -68,19 +79,6 @@ var newDragon = function(event){
 	document.getElementById('displayBox').appendChild(dragon);
 }//end newDragon
 
-/* YO NOTES ABOUT STUFF FOR LATER ON
-	probably we want to change it so that it's the IMAGE that has the listener
-	oh and how are we gonna do colors
-	might be easier to just have roygbiv images, no sure if i can recolor without saving the image which is dumb
-	
-	cool stuff 2 add:
-	-pull to the top to keep bbs from being buried
-	-delete drg
-	show/hide add menu
-	-stop the jump on clicking - would need to save initial mouse position
-*/
-
-
 //click and drag a dragon on the screen
 var move = function(event){
 	var selection = this;
@@ -88,6 +86,7 @@ var move = function(event){
 		//change the top and left vals of the div to match
 		selection.parentNode.style.top = event.pageY-(selection.height/3);
 		selection.parentNode.style.left = event.pageX-(selection.width/2);
+		
 	}//end document mousemove
 	
 
@@ -102,6 +101,37 @@ var move = function(event){
 	}//end div mouseup
 	
 }//end move
+
+//okay maybe check if this is wing or body
+//blegh maybe i should just redo the wing part entirely
+
+var moveWing = function(event){
+	var selection = this;
+	var body = this.parentNode.parentNode;
+	console.log("hup");
+	document.onmousemove = function(event){
+		//change the top and left vals of the div to match
+		//YO FIX DIS
+		selection.parentNode.parentNode.style.top = event.pageY;
+		selection.parentNode.parentNode.style.left = event.pageX;
+		
+	}//end document mousemove
+	
+
+	this.onmouseup = function(event){
+		//check if its on the delete box
+		if(event.pageY >= window.innerHeight-100 && event.pageX <= 100){			
+			this.parentNode.remove();
+		}
+		
+		//clear the mousemove function until we trigger move again	
+		document.onmousemove = null;
+	}//end div mouseup
+	
+}//end move
+
+
+
 
 
 //show/hide create dragon menu

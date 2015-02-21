af5187fd114c8279bc5bbda6c67c02b600335441
emcall <esm328@nyu.edu>
2015-02-21 00:13:03 -0500

    probably not how this is sipposed to work
    I still barely understand branches. what happens if i just shove all my
    files from master in here??? i thought that was what's supposed to
    happen with a branch why is github the way it is

---
+++ sizes.js
@@ -0,0 +1,153 @@
+
+//create new dragon
+var newDragon = function(event){
+	//getting the input values
+	var d_breed = document.getElementById("breed").value;
+	var d_length = document.getElementById("length").value;
+	var d_wingspan = document.getElementById("wingspan").value;
+	var d_name = document.getElementById("name").value;
+	
+	//make sure all values are filled (name is optional)
+	if(!d_length || !d_breed || !d_wingspan){
+		return;
+	}
+	
+	//clear the form
+	document.getElementById("breed").value = "";
+	document.getElementById("length").value = "";
+	document.getElementById("wingspan").value = "";
+	document.getElementById("name").value = "";
+	
+	//create the div for the dragon
+	var dragon = document.createElement('div');
+	dragon.className = "dragonBody";
+	
+	//initial positioning
+	dragon.style.top = "50px";
+	dragon.style.left = "500px";
+	dragon.style.zIndex = 1;
+	
+	//add image
+	var dragonImage = document.createElement("img");
+	dragonImage.src = "images/" + d_breed + "_body.png";	
+	dragonImage.height = d_length; 
+	dragonImage.zIndex = 1;
+	dragon.appendChild(dragonImage);
+	dragonImage.addEventListener('mousedown', move);
+	
+	
+	//wing div
+	var wings = document.createElement('div');
+	wings.className = "wings";
+	//position the wing to center
+	
+	wings.style.left = -d_wingspan/2 + d_length/6;
+	wings.style.top = 0;
+	
+	//this pushes the wings down the shoulders
+	//i need a better calculation - have to determine the shoulder location
+	wings.style.paddingTop = d_length / 8;
+	
+	var wingImage = document.createElement("img");
+	wingImage.src = "images/" + d_breed + "_wings.png";	
+	wingImage.width = d_wingspan; 
+	wingImage.style.zIndex = "-1";
+	wings.appendChild(wingImage);	
+	dragon.appendChild(wings);
+	
+
+	//this adds the text
+	
+	var label = document.createElement("p");
+	label.textContent = d_name;
+	label.className = "label";
+	label.style.top = d_length/4;
+	label.style.width = d_wingspan;
+	label.style.zIndex= 2;
+	
+	wings.appendChild(label);
+	
+	wingImage.addEventListener('mousedown', moveWing);
+	
+	dragon.appendChild(wings);
+	
+	
+	//remove the normal drag function
+	dragon.ondragstart = function() { return false };
+	
+	//add div to display box
+	document.getElementById('displayBox').appendChild(dragon);
+}//end newDragon
+
+//click and drag a dragon on the screen
+var move = function(event){
+	var selection = this;
+	document.onmousemove = function(event){
+		//change the top and left vals of the div to match
+		selection.parentNode.style.top = event.pageY-(selection.height/3);
+		selection.parentNode.style.left = event.pageX-(selection.width/2);
+		
+	}//end document mousemove
+	
+
+	this.onmouseup = function(event){
+		//check if its on the delete box
+		if(event.pageY >= window.innerHeight-100 && event.pageX <= 100){			
+			this.parentNode.remove();
+		}
+		
+		//clear the mousemove function until we trigger move again	
+		document.onmousemove = null;
+	}//end div mouseup
+	
+}//end move
+
+
+
+//if the wing image is clicked on, move needs to function a little differently
+//We need to move the dragon div (the parent(dragon) of the parent(wing))
+var moveWing = function(event){
+	var selection = this;
+	var dragonbody = this.parentNode.parentNode;
+	document.onmousemove = function(event){
+		dragonbody.style.top = event.pageY-selection.width/2;
+		dragonbody.style.left = event.pageX-selection.height/2;
+		
+	}//end document mousemove
+	
+	this.onmouseup = function(event){
+		//check if its on the delete box
+		if(event.pageY >= window.innerHeight-100 && event.pageX <= 100){			
+			this.parentNode.remove();
+		}	
+		//clear the mousemove function until we trigger move again	
+		document.onmousemove = null;
+	}//end div mouseup
+	
+}//end moveWing
+
+
+var changeBG = function(event){
+	var color = document.getElementById("color").value;
+	if(color[0] != "#")
+		color = "#" + color;	
+	document.body.style.background = color;	
+}
+
+
+
+
+
+//show/hide create dragon menu
+var toggleForm = function(event){
+	var form = document.getElementById("form");
+	if(form.style.display === "block"){
+		form.style.display = "none";
+
+	}
+	else{
+		form.style.display = "block";
+
+	}
+}
+
